f221f602580e994b1cbe3a1f1255b44dd5a27d8a
candripanjaitan16 <candripanjaitan16@gmail.com>
2026-05-23 11:21:36 +0700

    chore: setup dual deployment base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: mode === "github" ? "/batak/" : "/",
+// Perhatikan kurung kurawal { mode } di bawah ini, itu kuncinya!
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === "github" ? "/batak/" : "/",
+  };
 });
